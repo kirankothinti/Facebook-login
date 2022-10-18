4fd0fff7678e3cc4d62cef8e934bb7162bffc22d
kiran <kiran.kothinti@prutech.com>
2022-10-18 10:25:24 +0530

    Commiting with code

---
+++ facebook-login-app/src/main/java/com/login/facebookloginapp/FacebookLoginAppApplication.java
@@ -0,0 +1,13 @@
+package com.login.facebookloginapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacebookLoginAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacebookLoginAppApplication.class, args);
+	}
+
+}
---
+++ facebook-login-app/src/test/java/com/login/facebookloginapp/FacebookLoginAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login.facebookloginapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacebookLoginAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
